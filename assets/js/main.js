78fc84bfda23133e4d53682b0eee44d35532c16c
Kashyap297 <kashyap29700@gmail.com>
2024-11-26 01:03:43 +0530

    footer done - & home page successfully done

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,4 +25,15 @@ document.addEventListener("DOMContentLoaded", () => {
             })
             .catch((error) => console.error("Error loading header:", error));
     }
+     // Load Footer
+     const footerElement = document.getElementById("footer");
+     if (footerElement) {
+         fetch("/assets/components/footer.html")
+             .then((response) => response.text())
+             .then((html) => {
+                 footerElement.innerHTML = html;
+                 // Optional: Add footer-specific JS logic here
+             })
+             .catch((error) => console.error("Error loading footer:", error));
+     }
 });
